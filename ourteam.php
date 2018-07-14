24c2d0c1e379ef0ce68a46f0fb4e33c314546666
alexwaite <alexcwaite@gmail.com>
2018-07-13 20:51:38 -0400

    changed name of team php file, updated wording for fields

--- ourteam.php
+++ ourteam.php
@@ -1,4 +1,4 @@
-<?php /* Template Name: Team Members Page Template */ ?>
+<?php /* Template Name: Board & Staff Template */ ?>
 <?php get_header(); ?>
 
 
@@ -17,17 +17,17 @@ endwhile; // End of the loop.
 
 <!-- Custom Post Type Loop (with 'team' as the post type 'slug') -->
 <?php
-$args = array( 'post_type' => 'team', 'posts_per_page' => -1 );
+$args = array( 'post_type' => 'staff', 'posts_per_page' => -1 );
 $loop = new WP_Query( $args );
 while ( $loop->have_posts() ) : $loop->the_post(); ?>
 
 
 	<hr>
 	<h2 class="name"><?php the_title(); ?></h2>
-	<img src="<?php the_field('tm_img'); ?>">
-	<h3><?php the_field('tm_name'); ?></h3>
-	<h4><?php the_field('tm_title'); ?></h4>
-	<p><?php the_field('tm_bio'); ?></p>
+	<img src="<?php the_field('sm_img'); ?>">
+	<h3><?php the_field('sm_name'); ?></h3>
+	<h4><?php the_field('sm_title'); ?></h4>
+	<p><?php the_field('sm_bio'); ?></p>
 
 	<hr>
 
