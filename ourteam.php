81db4827cca3507aa85557118b22b3bb7fb6fb30
alexwaite <alexcwaite@gmail.com>
2018-07-17 18:07:03 -0400

    updated ourteam.php and stylesheet

--- ourteam.php
+++ ourteam.php
@@ -15,7 +15,7 @@ endwhile; // End of the loop.
 ?>
 
 
-<!-- Custom Post Type Loop (with 'team' as the post type 'slug') -->
+<!-- Custom Post Type Loop (with 'staff' as the post type 'slug') -->
 <?php
 $args = array( 'post_type' => 'staff', 'posts_per_page' => -1 );
 $loop = new WP_Query( $args );
@@ -23,11 +23,21 @@ while ( $loop->have_posts() ) : $loop->the_post(); ?>
 
 
 	<hr>
-	<h2 class="name"><?php the_title(); ?></h2>
-	<img src="<?php the_field('sm_img'); ?>">
-	<h3><?php the_field('sm_name'); ?></h3>
-	<h4><?php the_field('sm_title'); ?></h4>
-	<p><?php the_field('sm_bio'); ?></p>
+
+	<div class="card-container clearfix">
+
+		<div class="staff-image">
+			<img class="sm_img" src="<?php the_field('sm_img'); ?>">
+		</div>
+
+		<div class="staff-card">
+			<p class="sm_name"><?php the_field('sm_name'); ?></p>
+			<p class="sm_title"><?php the_field('sm_title'); ?></p>
+			<p class="sm_bio"><?php the_field('sm_bio'); ?></p>
+		</div>
+
+	</div>
+
 
 	<hr>
 
